8e21588ad83708f7be7c68c9689d1d8a2e821bf0
Zahrexxs <azzahri17@gmail.com>
2024-04-19 02:00:43 +0700

    first commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'Beranda.dart'; // Pastikan Anda mengimpor file Beranda.dart dengan benar
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Menunda navigasi ke tampilan Beranda setelah 2 detik
+    Timer(const Duration(seconds: 2), () {
+      // Navigasi ke Beranda dan ganti tata letak (layout) dengan menggantikan
+      // SplashScreen dari tumpukan navigasi
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          transitionDuration:
+              const Duration(milliseconds: 1000), // Durasi transisi
+          pageBuilder: (_, __, ___) =>
+              Beranda(key: UniqueKey()), // Tambahkan key di sini
+          transitionsBuilder: (_, animation, __, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
+      );
+    });
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/Loading Screen.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
